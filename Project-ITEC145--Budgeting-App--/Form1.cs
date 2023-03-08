137dcb1b3728f394ce46ebb574bb0f8a00cc126c
Thomas Gander - School <thomas.gander@mymhc.ca>
2023-03-08 11:46:03 -0700

    Add project files.

---
+++ Project-ITEC145--Budgeting-App--/Form1.cs
@@ -0,0 +1,10 @@
+namespace Project_ITEC145__Budgeting_App__
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
