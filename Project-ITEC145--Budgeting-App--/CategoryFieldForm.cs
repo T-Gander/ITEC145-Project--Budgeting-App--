92429c9d20e39500173a4fcb56c3ae5b41936707
Thomas Gander - School <thomas.gander@mymhc.ca>
2023-03-09 13:04:31 -0700

    Added a category form

---
+++ Project-ITEC145--Budgeting-App--/CategoryFieldForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_ITEC145__Budgeting_App__
+{
+    public partial class CategoryFieldForm : Form
+    {
+        public CategoryFieldForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
