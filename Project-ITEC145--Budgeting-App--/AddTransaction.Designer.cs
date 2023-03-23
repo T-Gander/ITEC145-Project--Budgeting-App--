0e8fe593af7dc030dd5ba0dc9a97bc22014ec0cb
Thomas Gander <Thomas.Gander@mymhc.ca>
2023-03-23 01:36:40 -0600

    Add transaction form basically ready other than being linked to the budgetsheet as a static form.

--- Project-ITEC145--Budgeting-App--/AddTransaction.Designer.cs
+++ Project-ITEC145--Budgeting-App--/AddTransaction.Designer.cs
@@ -28,121 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.SuspendLayout();
+            label1 = new Label();
+            label2 = new Label();
+            txtName = new TextBox();
+            txtAmount = new TextBox();
+            btnAddTransaction = new Button();
+            btnCancel = new Button();
+            rdoCredit = new RadioButton();
+            rdoDebit = new RadioButton();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Transaction Name";
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Transaction Name";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(338, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 15);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Amount $.$$";
+            label2.AutoSize = true;
+            label2.Location = new Point(338, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Amount $.$$";
             // 
-            // textBox1
+            // txtName
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 27);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(226, 23);
-            this.textBox1.TabIndex = 2;
+            txtName.Location = new Point(12, 27);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(226, 23);
+            txtName.TabIndex = 2;
             // 
-            // textBox2
+            // txtAmount
             // 
-            this.textBox2.Location = new System.Drawing.Point(338, 27);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(99, 23);
-            this.textBox2.TabIndex = 3;
+            txtAmount.Location = new Point(338, 27);
+            txtAmount.Name = "txtAmount";
+            txtAmount.Size = new Size(99, 23);
+            txtAmount.TabIndex = 3;
             // 
-            // button1
+            // btnAddTransaction
             // 
-            this.button1.Location = new System.Drawing.Point(12, 112);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Add Transaction";
-            this.button1.UseVisualStyleBackColor = true;
+            btnAddTransaction.Location = new Point(12, 112);
+            btnAddTransaction.Name = "btnAddTransaction";
+            btnAddTransaction.Size = new Size(125, 23);
+            btnAddTransaction.TabIndex = 4;
+            btnAddTransaction.Text = "Add Transaction";
+            btnAddTransaction.UseVisualStyleBackColor = true;
+            btnAddTransaction.Click += button1_Click;
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(338, 112);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(125, 23);
-            this.btnCancel.TabIndex = 5;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            btnCancel.Location = new Point(338, 112);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(125, 23);
+            btnCancel.TabIndex = 5;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
             // 
-            // radioButton1
+            // rdoCredit
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(20, 56);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(172, 19);
-            this.radioButton1.TabIndex = 6;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Credit (Positive Transaction)";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            rdoCredit.AutoSize = true;
+            rdoCredit.Location = new Point(20, 56);
+            rdoCredit.Name = "rdoCredit";
+            rdoCredit.Size = new Size(172, 19);
+            rdoCredit.TabIndex = 6;
+            rdoCredit.TabStop = true;
+            rdoCredit.Text = "Credit (Positive Transaction)";
+            rdoCredit.UseVisualStyleBackColor = true;
+            rdoCredit.CheckedChanged += rdoCredit_CheckedChanged;
             // 
-            // radioButton2
+            // rdoDebit
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(20, 81);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(174, 19);
-            this.radioButton2.TabIndex = 7;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Debit (Negative Transaction)";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            rdoDebit.AutoSize = true;
+            rdoDebit.Location = new Point(20, 81);
+            rdoDebit.Name = "rdoDebit";
+            rdoDebit.Size = new Size(174, 19);
+            rdoDebit.TabIndex = 7;
+            rdoDebit.TabStop = true;
+            rdoDebit.Text = "Debit (Negative Transaction)";
+            rdoDebit.UseVisualStyleBackColor = true;
+            rdoDebit.CheckedChanged += rdoDebit_CheckedChanged;
             // 
             // AddTransaction
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(475, 147);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.MinimizeBox = false;
-            this.Name = "AddTransaction";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Add a Transaction!";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(475, 147);
+            Controls.Add(rdoDebit);
+            Controls.Add(rdoCredit);
+            Controls.Add(btnCancel);
+            Controls.Add(btnAddTransaction);
+            Controls.Add(txtAmount);
+            Controls.Add(txtName);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            MinimizeBox = false;
+            Name = "AddTransaction";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Add a Transaction!";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Label label1;
         private Label label2;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private Button button1;
+        private TextBox txtName;
+        private TextBox txtAmount;
+        private Button btnAddTransaction;
         private Button btnCancel;
-        private RadioButton radioButton1;
-        private RadioButton radioButton2;
+        private RadioButton rdoCredit;
+        private RadioButton rdoDebit;
     }
 }
--- Project-ITEC145--Budgeting-App--/AddTransaction.cs
+++ Project-ITEC145--Budgeting-App--/AddTransaction.cs
@@ -12,19 +12,34 @@ namespace Project_ITEC145__Budgeting_App__
 {
     public partial class AddTransaction : Form
     {
+        private bool Credit = false;
+        private bool Debit = false;
+
         public AddTransaction()
         {
             InitializeComponent();
         }
 
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        private void rdoCredit_CheckedChanged(object sender, EventArgs e)
         {
-
+            Credit = true;
+            Debit = false;
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void rdoDebit_CheckedChanged(object sender, EventArgs e)
+        {
+            Debit = true;
+            Credit = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
