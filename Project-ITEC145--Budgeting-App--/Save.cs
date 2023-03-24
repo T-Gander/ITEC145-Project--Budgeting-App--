5d28198b0c535bf54e5ecd0b1f6322a643e5ee29
Thomas Gander - School <thomas.gander@mymhc.ca>
2023-03-24 11:02:43 -0600

    Thinking about what variables I need to save

--- Project-ITEC145--Budgeting-App--/Save.cs
+++ Project-ITEC145--Budgeting-App--/Save.cs
@@ -9,10 +9,15 @@ namespace Project_ITEC145__Budgeting_App__
     [Serializable]
     public class Save
     {
+        private int _categoryCount = 0;
+        private int _budgetSheetCount = 0;
+        private string _budgetSheetName = "";
+        private List<MyTransactionsSheet> _transactions = new List<MyTransactionsSheet>();
+
         Save(BudgetSheet budgetSheet)
         {
             //Code to save control data
-
+            
         }
     }
 }
