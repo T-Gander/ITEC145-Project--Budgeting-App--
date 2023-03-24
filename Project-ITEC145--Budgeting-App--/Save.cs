17f84e283094c66b354ad4f3b7c9c48244400c63
Thomas Gander - School <thomas.gander@mymhc.ca>
2023-03-24 10:59:50 -0600

    Added save class

---
+++ Project-ITEC145--Budgeting-App--/Save.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_ITEC145__Budgeting_App__
+{
+    [Serializable]
+    public class Save
+    {
+        Save(BudgetSheet budgetSheet)
+        {
+            //Code to save control data
+
+        }
+    }
+}
